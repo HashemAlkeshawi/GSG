b074cde3a7d9a9fd71e58445ecd687e82b7ab821
Hashem Alkeshawi <hashemalkeshawi@gmail.com>
2022-06-27 22:23:06 +0300

    facebook_ui

---
+++ first_app_gsg/lib/FaceBook_UI/FaceBook2_ui.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class FaceBook2_ui extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return 
+  }
+}
--- first_app_gsg/lib/FaceBook_UI/FaceBook_ui.dart
+++ first_app_gsg/lib/FaceBook_UI/FaceBook_ui.dart
@@ -4,6 +4,39 @@ class FacebookUi extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      bottomNavigationBar: Container(
+        // padding: EdgeInsets.only(left: 12, right: 12, bottom: 12),
+        margin: EdgeInsets.only(bottom: 12),
+        height: 40,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Icon(
+              Icons.web,
+              color: Colors.blue,
+              size: 35,
+            ),
+            Icon(
+              Icons.group_outlined,
+              color: Colors.grey[600],
+              size: 35,
+            ),
+            Icon(
+              Icons.smart_display_outlined,
+              color: Colors.grey[600],
+              size: 35,
+            ),
+            Icon(
+              Icons.notifications_outlined,
+              color: Colors.grey[600],
+              size: 35,
+            ),
+            CircleAvatar(
+              backgroundImage: AssetImage("assets/images/me.png"),
+            )
+          ],
+        ),
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           print("Add Post!");
@@ -35,7 +68,7 @@ class FacebookUi extends StatelessWidget {
           decoration: BoxDecoration(
               color: Color.fromARGB(255, 231, 231, 231),
               border: Border.all(
-                color: Colors.grey,
+                color: Color.fromARGB(255, 205, 205, 205),
                 width: 1,
               ),
               borderRadius: BorderRadius.all(
@@ -402,49 +435,244 @@ class FacebookUi extends StatelessWidget {
                   ),
                 ),
               ),
-              Container(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    ListTile(
-                      contentPadding: EdgeInsets.all(0),
-                      horizontalTitleGap: 4,
-                      leading: CircleAvatar(
-                        backgroundImage: AssetImage("assets/images/me.png"),
-                      ),
-                      title: SizedBox(
-                        child: Row(
-                          children: [
-                            Text(
-                              "Hashem Alkeshawi",
-                              style: TextStyle(
-                                  fontSize: 10, fontWeight: FontWeight.bold),
+              Column(
+                children: [
+                  Container(
+                    margin: EdgeInsets.symmetric(vertical: 12),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        ListTile(
+                          contentPadding: EdgeInsets.all(0),
+                          horizontalTitleGap: 4,
+                          leading: CircleAvatar(
+                            backgroundImage: AssetImage("assets/images/me.png"),
+                          ),
+                          title: SizedBox(
+                            child: Row(
+                              children: [
+                                Text(
+                                  "Hashem Alkeshawi",
+                                  style: TextStyle(
+                                      fontSize: 10,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                Text(
+                                  " updated his cover photo",
+                                  style: TextStyle(fontSize: 10),
+                                ),
+                              ],
                             ),
-                            Text(
-                              " updated his cover photo",
-                              style: TextStyle(fontSize: 10),
+                          ),
+                          subtitle: Text(
+                            "3 mins ago",
+                            style: TextStyle(height: 1.5),
+                          ),
+                          trailing: InkWell(
+                            child: Icon(Icons.more_horiz),
+                            onTap: () {
+                              print("more about post");
+                            },
+                          ),
+                        ),
+                        Container(
+                          height: 200,
+                          width: double.infinity,
+                          margin: EdgeInsets.symmetric(vertical: 10),
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(20),
+                              image: DecorationImage(
+                                  image: AssetImage("assets/images/coder.jpg"),
+                                  fit: BoxFit.cover)),
+                        ),
+                        Row(
+                          children: [
+                            Icon(
+                              Icons.recommend,
+                              color: Colors.blue,
+                            ),
+                            InkWell(
+                              onTap: () {
+                                print("Reactions");
+                              },
+                              child: Text(
+                                "  109",
+                                style: TextStyle(fontSize: 10),
+                              ),
+                            ),
+                            Spacer(),
+                            InkWell(
+                              onTap: () {
+                                print("Commints");
+                              },
+                              child: Text(
+                                "73 commints",
+                                style: TextStyle(fontSize: 10),
+                              ),
                             ),
                           ],
                         ),
-                      ),
-                      subtitle: Text(
-                        "3 mins ago",
-                        style: TextStyle(height: 1.5),
-                      ),
-                      trailing: InkWell(
-                        child: Icon(Icons.more_horiz),
-                        onTap: () {
-                          print("more about post");
-                        },
-                      ),
+                        Divider(),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            InkWell(
+                              onTap: () {
+                                print("Liked the post");
+                              },
+                              child: Row(
+                                children: [
+                                  Icon(
+                                    Icons.thumb_up_alt_outlined,
+                                  ),
+                                  Text(
+                                    "  Like",
+                                    style: TextStyle(fontSize: 13),
+                                  )
+                                ],
+                              ),
+                            ),
+                            InkWell(
+                              onTap: () {
+                                print("Liked the post");
+                              },
+                              child: Row(
+                                children: [
+                                  Icon(
+                                    Icons.chat_bubble_outline,
+                                  ),
+                                  Text(
+                                    "  Commint",
+                                    style: TextStyle(fontSize: 13),
+                                  )
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
                     ),
-                    Container(
-                      child: Image.asset("assets/images/coder.jpg"),
+                  ),
+                  Divider(
+                    height: 20,
+                  ),
+                  Container(
+                    margin: EdgeInsets.symmetric(vertical: 12),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        ListTile(
+                          contentPadding: EdgeInsets.all(0),
+                          horizontalTitleGap: 4,
+                          leading: CircleAvatar(
+                            backgroundImage: AssetImage("assets/images/me.png"),
+                          ),
+                          title: SizedBox(
+                            child: Row(
+                              children: [
+                                Text(
+                                  "Hashem Alkeshawi",
+                                  style: TextStyle(
+                                      fontSize: 10,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                Text(
+                                  " updated his cover photo",
+                                  style: TextStyle(fontSize: 10),
+                                ),
+                              ],
+                            ),
+                          ),
+                          subtitle: Text(
+                            "3 mins ago",
+                            style: TextStyle(height: 1.5),
+                          ),
+                          trailing: InkWell(
+                            child: Icon(Icons.more_horiz),
+                            onTap: () {
+                              print("more about post");
+                            },
+                          ),
+                        ),
+                        Container(
+                          height: 200,
+                          width: double.infinity,
+                          margin: EdgeInsets.symmetric(vertical: 10),
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(20),
+                              image: DecorationImage(
+                                  image: AssetImage("assets/images/coder.jpg"),
+                                  fit: BoxFit.cover)),
+                        ),
+                        Row(
+                          children: [
+                            Icon(
+                              Icons.recommend,
+                              color: Colors.blue,
+                            ),
+                            InkWell(
+                              onTap: () {
+                                print("Reactions");
+                              },
+                              child: Text(
+                                "  109",
+                                style: TextStyle(fontSize: 10),
+                              ),
+                            ),
+                            Spacer(),
+                            InkWell(
+                              onTap: () {
+                                print("Commints");
+                              },
+                              child: Text(
+                                "73 commints",
+                                style: TextStyle(fontSize: 10),
+                              ),
+                            ),
+                          ],
+                        ),
+                        Divider(),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            InkWell(
+                              onTap: () {
+                                print("Liked the post");
+                              },
+                              child: Row(
+                                children: [
+                                  Icon(
+                                    Icons.thumb_up_alt_outlined,
+                                  ),
+                                  Text(
+                                    "  Like",
+                                    style: TextStyle(fontSize: 13),
+                                  )
+                                ],
+                              ),
+                            ),
+                            InkWell(
+                              onTap: () {
+                                print("Liked the post");
+                              },
+                              child: Row(
+                                children: [
+                                  Icon(
+                                    Icons.chat_bubble_outline,
+                                  ),
+                                  Text(
+                                    "  Commint",
+                                    style: TextStyle(fontSize: 13),
+                                  )
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
                     ),
-                    Row(),
-                    Row(),
-                  ],
-                ),
+                  ),
+                ],
               ),
             ],
           ),
